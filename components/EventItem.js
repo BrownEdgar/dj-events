7fbd910b8365e1d546d07d4a1e91cc259ca67d23
BrownEdgar <nikogosjanedgar@gmail.com>
2022-03-09 12:05:51 +0400

    add Events

---
+++ components/EventItem.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
+import MainLayouts from "./MainLayouts";
+import styles from "@/styles/EventItem.module.css"
+
+
+export default function EventItem() {
+	return (
+		<MainLayouts>
+			<Link></Link>
+		</MainLayouts>
+	)
+}
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,16 @@ import React from 'react'
 import Link from 'next/link'
 import MainLayouts from '@/components/MainLayouts'
 import { API_URL } from '@/config/index'
-export default function Homepage() {
+export default function Homepage({events}) {
 	return (
 		<MainLayouts>
 			<h1>Home page s</h1>
+			{events.length === 0 && <h3>No Events Show</h3>}
+			{events.map((elem => {
+				return  (
+					<h3 key={elem.id}>{elem.name}</h3>
+				)
+			}))}
 		</MainLayouts>
 	
 	)
